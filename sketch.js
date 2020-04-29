5a48c13db672a805534d450b22094a8d93ba376b
Jessica Lu <jl10698@nyu.edu>
2020-04-29 19:03:48 -0400

    Add images, start first room
    Added an images folder with assets, started coding first puzzle

--- sketch.js
+++ sketch.js
@@ -1,8 +1,54 @@
+let isMapPress= false;
+let isDoorPress= false;
+let isboxPress= false;
+let isboxSolved= false;
+let backButton= false;
+
+function preload(){
+    room1 = loadImage('Images/hotel bedroom.jpg');
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    background('#252B42');
+    
+    room1.loadPixels();
+   
 }
 
 function draw() {
-    background(200);
-    rect(0, 0, 100, 100);
+    image(room1, (windowWidth/2)-350, (windowHeight/2)-275, 700, 550);
+    if(mouseX>=970 && mouseX<=1050 && mouseY>=260 && mouseY<=430){
+        cursor(HAND);
+    }
+    else{
+        cursor(ARROW);
+    }
+}
+
+function mousePressed(){
+    if(mouseX>=970 && mouseX<=1050 && mouseY>=260 && mouseY<=430){
+        cursor(HAND);
+        alert("Locked!")
+    }
+    else{
+        cursor(ARROW);
+    }
+}
+
+function clickDoor(){
+        
+    
+}
+
+function clickMap(){
+
+}
+
+function clickBox(){
+
+}
+
+function boxPuzzle(){
+
 }
